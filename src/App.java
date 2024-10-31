bde063f4558f38ca1a73c29c49a1a3cb9e40bd76
V26R47 <28vrichmondshazeer@gannacademy.org>
2024-10-31 13:00:52 -0400

    done

--- src/App.java
+++ src/App.java
@@ -8,6 +8,16 @@ public class App {
     //   I add more elements to your ArrayList. For example, I might add 7 to the
     //   ArrayList and this should still work.
     public static void main(String[] args) throws Exception {
+    Scanner scanner = new Scanner(System.in);    
+    ArrayList<Integer> list = new ArrayList<>();
+    list.add(6);
+    list.add(3);
+    list.add(1);
+    list.add(2);
+    list.add(7);
+    for (int i = 0; i < list.size(); i++) {
         
+        System.out.println(list.get(i));
+    }
     }
 }
